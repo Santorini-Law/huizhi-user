bb824aba91db85fc146ce0b456a7ab7ee5a10bb7
YinglishZhi <yinglishzhi@gmail>
2020-03-08 16:03:31 +0800

    user

---
+++ src/main/java/com/zhihui/user/controller/v1/test.java
@@ -0,0 +1,23 @@
+package com.zhihui.user.controller.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 1
+ *
+ * @author LDZ
+ * @date 2020-03-04 15:43
+ */
+@RestController
+@RequestMapping(value = "interest")
+public class test {
+
+    @ResponseBody
+    @GetMapping("/public/user")
+    public String hello() {
+        return "hello";
+    }
+}
